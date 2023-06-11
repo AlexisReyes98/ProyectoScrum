2f13858915875d6667fb96c7e28610220ca5cd66
AlexisReyes98 <giovanny29.garv@gmail.com>
2023-06-11 01:18:12 -0600

    Sprint 3 Cliente terminado

--- src/View/GUI_Login/Login.java
+++ src/View/GUI_Login/Login.java
@@ -128,8 +128,8 @@ public class Login extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        //new MenuPrincipalCliente().setVisible(true);
-        //this.setVisible(false);
+        new MenuPrincipalCliente().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
